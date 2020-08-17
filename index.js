fc8482e9392cafa21ccf60d33936a4b3024d980a
assam06 <67585123+assam06@users.noreply.github.com>
2020-08-17 14:06:04 +0900

    update_index.js
    hey

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const title = document.getElementById("title");
 
-console.log(title);
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+console.dir(document);
 
-DOM;
+/* console.log(title);-> 이건 콘솔에서 title에 해당하는
+html을 볼 것임*/
+
 /*js는 내 html에 있는 모든 요소(태그)를 가져와서 객체로 만들 것임
 객체는 console.log의 log처럼 점찍으면 쭉 나옴*/
